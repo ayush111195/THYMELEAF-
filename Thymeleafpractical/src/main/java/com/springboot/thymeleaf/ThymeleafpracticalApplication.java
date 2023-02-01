264b00463852e93f658e0b5ea86e64e3e20e2049
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-02-02 01:30:57 +0530

    Add files via upload

---
+++ Thymeleafpractical/src/main/java/com/springboot/thymeleaf/ThymeleafpracticalApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafpracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafpracticalApplication.class, args);
+	}
+
+}
---
+++ Thymeleafpractical/src/main/java/com/springboot/thymeleaf/controller/Controller.java
@@ -0,0 +1,54 @@
+package com.springboot.thymeleaf.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+	
+	@GetMapping("/about")
+	
+	public String about(Model model)
+	{
+	System.out.println("INSIDE THE ABOUT HANDLER");
+	
+	model.addAttribute("name", "Ayush");
+	model.addAttribute("currentDate", new Date().toLocaleString());
+
+		return "about" ;
+		//about.html
+	}
+	
+	//HANDLING ITERATION---->>>
+	
+	@GetMapping("/example-loop")
+	public String iterateHandler(Model m)
+	{
+		//CREATE A LIST--->>
+		List<String> names = List.of("AYUSH,ANKIT,ARPIT,AMAN");
+		m.addAttribute("listofname", names);
+		return "iterate";
+		
+	}
+	
+	 //HANDLER FOR CONDITIONAL---->>>
+	
+	@GetMapping("/condition")
+	public String conditinolalHandler( Model m)
+	{
+		System.out.println("//HANDLER FOR CONDITIONAL---->>>");
+		
+		m.addAttribute("isActive", true);
+		m.addAttribute("gander", "f");
+		List<Integer> list=List.of(333,877,656,776);
+		m.addAttribute("mylist", list);
+		return"condition";
+		
+	}
+}
