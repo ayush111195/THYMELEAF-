9c9735655ebb9a4a4f51adb154a61997bba7944c
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-02-02 23:35:03 +0530

    Add files via upload

--- Thymeleafpractical/src/main/java/com/springboot/thymeleaf/controller/Controller.java
+++ Thymeleafpractical/src/main/java/com/springboot/thymeleaf/controller/Controller.java
@@ -11,44 +11,67 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-	
-	@GetMapping("/about")
-	
-	public String about(Model model)
-	{
-	System.out.println("INSIDE THE ABOUT HANDLER");
-	
-	model.addAttribute("name", "Ayush");
-	model.addAttribute("currentDate", new Date().toLocaleString());
 
-		return "about" ;
-		//about.html
+	@GetMapping("/about")
+
+	public String about(Model model) {
+		System.out.println("INSIDE THE ABOUT HANDLER");
+
+		model.addAttribute("name", "Ayush");
+		model.addAttribute("currentDate", new Date().toLocaleString());
+
+		return "about";
+		// about.html
 	}
-	
-	//HANDLING ITERATION---->>>
-	
+
+	// HANDLING ITERATION---->>>
+
 	@GetMapping("/example-loop")
-	public String iterateHandler(Model m)
-	{
-		//CREATE A LIST--->>
+	public String iterateHandler(Model m) {
+		// CREATE A LIST--->>
 		List<String> names = List.of("AYUSH,ANKIT,ARPIT,AMAN");
 		m.addAttribute("listofname", names);
 		return "iterate";
-		
+
 	}
-	
-	 //HANDLER FOR CONDITIONAL---->>>
-	
+
+	// HANDLER FOR CONDITIONAL---->>>
+
 	@GetMapping("/condition")
-	public String conditinolalHandler( Model m)
-	{
+	public String conditinolalHandler(Model m) {
 		System.out.println("//HANDLER FOR CONDITIONAL---->>>");
-		
+
 		m.addAttribute("isActive", true);
 		m.addAttribute("gander", "f");
-		List<Integer> list=List.of(333,877,656,776);
+		List<Integer> list = List.of(333, 877, 656, 776);
 		m.addAttribute("mylist", list);
-		return"condition";
+		return "condition";
+
+	}
+
+	//handler for {including fragments}--->>>
+	@GetMapping("service")
+	public String servicesHandleer(Model m)
+	{
+		m.addAttribute("tittle","this is tittle page");
+		m.addAttribute("about","this is about page");
+		return "service";
 		
 	}
+	
+	//for about  new
+	@GetMapping("/newabout")
+	public String newAbout()
+	{
+		return "aboutNew";
+		
+	}
+	//for contact
+		@GetMapping("/contact")
+		public String contact()
+		{
+			return "contact";
+			
+		}
+		
 }
